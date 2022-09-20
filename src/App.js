8809fb050a989d1bb4c3c4c40553312862c3f3d5
Iván González <Ivan.g.e.017@gmail.com>
2022-09-20 13:06:11 -0600

    Add hero, headline cards, food and category components. Add data.js which contains all the data like pictures id category from each element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Category from "./Components/Category";
+import Food from "./Components/Food";
+import HeadlineCards from "./Components/HeadlineCards";
+import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
     </div>
   );
 }
